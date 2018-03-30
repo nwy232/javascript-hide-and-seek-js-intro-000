bb62c95becf92a3e861804f0996b0f61cdf7210e
Kate Schwarz <kateschwarz0010@gmail.com>
2018-03-30 12:06:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function getFirstSelector(selector){
+  const first=document.querySelector('selector');
+  return first;
+}
+
+function nestedTarget(){
+  
+}
+
+function increaseRankBy(n){
+  
+}
+
+function deepestChild(){
+  
+}
